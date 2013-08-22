a289b570911bcbd9876df942fc56277c87134293
dbould <dbould@gmail.com>
2013-08-22 12:03:16 +0100

    More changes to DB structure

--- sql/geoCacheBase.sql
+++ sql/geoCacheBase.sql
@@ -1,12 +1,14 @@
 #Create DB
 CREATE DATABASE `geo_cache` /*!40100 COLLATE 'utf8_unicode_ci' */;
 
-#category table - description of area user is spotting from
+#environment table - description of area user is spotting from
 CREATE TABLE `environment` (
 	`environment_id` INT(10) NOT NULL AUTO_INCREMENT,
 	`evnironment_name` VARCHAR(150) NOT NULL DEFAULT 'Unnamed Location',
 	PRIMARY KEY (`category_id`)
 )
+
+#category table - collective name for objects
 COMMENT='Area category; description of area user is spotting from'
 COLLATE='utf8_unicode_ci'
 ENGINE=MyISAM;
@@ -17,5 +19,58 @@ CREATE TABLE `category` (
 	PRIMARY KEY (`category_id`)
 )
 COMMENT='Name for a collection of objects'
+COLLATE='utf8_unicode_ci'
+ENGINE=MyISAM;
+
+#object table - definition for individual objects
+CREATE TABLE `object` (
+	`object_id` INT(10) NOT NULL AUTO_INCREMENT,
+	`object_name` VARCHAR(150) NOT NULL DEFAULT 'Unnamed Object',
+	PRIMARY KEY (`object_id`)
+)
+COMMENT='Living or inanimate object to record discovery of'
+COLLATE='utf8_unicode_ci'
+ENGINE=MyISAM;
+
+#Discovery table - individual submissions from users, links everything together
+CREATE TABLE `discovery` (
+	`discovery_id` INT(10) NOT NULL AUTO_INCREMENT,
+	`environment_id` INT(10) NOT NULL DEFAULT '0',
+	`category_id` INT(10) NOT NULL DEFAULT '0',
+	`object_id` INT(10) NOT NULL DEFAULT '0',
+	`object_size_id` INT(10) NOT NULL DEFAULT '0',
+	`location_id` INT(10) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`discovery_id`)
+)
+COMMENT='Table for recording individual discoveries of objects'
+COLLATE='utf8_unicode_ci'
+ENGINE=MyISAM;
+
+#Describes object sizes
+CREATE TABLE `object_size` (
+	`object_size_id` INT(10) NOT NULL AUTO_INCREMENT,
+	`object_size_description` VARCHAR(150) NOT NULL DEFAULT '',
+	PRIMARY KEY (`object_size_id`)
+)
+COMMENT='Record possible sizes for object'
+COLLATE='utf8_unicode_ci'
+ENGINE=MyISAM;
+
+#Object category linking table
+CREATE TABLE `object_category` (
+	`object_id` INT(10) NOT NULL DEFAULT '0',
+	`category_id` INT(10) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`object_id`, `category_id`)
+)
+COMMENT='Links objects to categories'
+COLLATE='utf8_unicode_ci'
+ENGINE=MyISAM;
+
+#Location date storage
+CREATE TABLE `location` (
+	`location_id` INT(10) NOT NULL AUTO_INCREMENT,
+	`geolocation` POINT NOT NULL,
+	PRIMARY KEY (`location_id`)
+)
 COLLATE='utf8_unicode_ci'
 ENGINE=MyISAM;
