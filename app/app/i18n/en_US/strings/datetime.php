32adbf9d084b5f81e71342f0391f8d564001458a
ecocache <mememe@me.com>
2013-09-17 16:42:44 +0100

    Composer and Mako

---
+++ app/app/i18n/en_US/strings/datetime.php
@@ -0,0 +1,21 @@
+<?php
+
+//---------------------------------------------
+// Language file used by mako\DateTime
+//---------------------------------------------
+
+return array
+(
+	'today'       => 'today',
+	'yesterday'   => 'yesterday',
+	'minute_ago'  => 'a minute ago',
+	'minutes_ago' => '%u minutes ago',
+	'hour_ago'    => 'an hour ago',
+	'hours_ago'   => '%u hours ago',
+	'day_ago'     => 'a day ago',
+	'days_ago'    => '%u days ago',
+	'week_ago'    => 'a week ago',
+	'weeks_ago'   => '%u weeks ago',
+);
+
+/** -------------------- End of file --------------------**/
