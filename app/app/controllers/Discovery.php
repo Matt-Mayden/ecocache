d6b3c04e5e8a4b077cf4369879030fa30d7c88fe
ecocache <mememe@me.com>
2013-09-17 23:47:15 +0100

    Discovery now accpets data in as long as key fields are set, other fields are optional.

--- app/app/controllers/Discovery.php
+++ app/app/controllers/Discovery.php
@@ -35,14 +35,30 @@ class Discovery extends \mako\Controller
 
     public function post_index()
     {
-        $post['environment_id'] = \mako\Input::post("environment_id", null);
-        $post['category_id'] = \mako\Input::post("category_id", null);
-        $post['object_id'] = \mako\Input::post("object_id", null);
-        $post['object_size_id'] = \mako\Input::post("size_id", null);
-        $post['object_colour'] = \mako\Input::post("colour_id", null);
-        $post['location_id'] = \mako\Input::post("location_id", null);
+        $rawPost = $GLOBALS['HTTP_RAW_POST_DATA'];
+
+        $post = json_decode($rawPost, true);
+
+        if(isset($post["size"]))
+        {
+            $post['object_size_id'] = $post["size"];
+            unset($post['size']);
+        }
+
+        if(isset($post["colour"]))
+        {
+            $post['object_colour'] = $post["colour"];
+            unset($post['colour']);
+        }
+
+        if(isset($post["position"]))
+        {
+            $post['object_position'] = $post["position"];
+            unset($post['position']);
+        }
 
         $this->model->putDiscovery($post);
+
     }
 }
 
