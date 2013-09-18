94e983b68fb63045e23babd5994810c7787b93d2
Richard He <shhe.biz@gmail.com>
2013-09-18 13:40:04 +0100

    removed unused sql files

--- sql/addImageFields.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `category`
-	ADD COLUMN `image` VARCHAR(50) NULL AFTER `category_name`;
-
-ALTER TABLE `environment`
-	ADD COLUMN `image` VARCHAR(50) NULL AFTER `environment_name`;
-
-ALTER TABLE `object`
-	ADD COLUMN `image` VARCHAR(50) NULL AFTER `object_name`;
--- sql/objectColAdditions.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `discovery`
-	ADD COLUMN `object_size_id` INT NOT NULL DEFAULT '0' AFTER `location_id`,
-	ADD COLUMN `object_colour` VARCHAR(150) NOT NULL DEFAULT '' AFTER `object_size_id`;
-
-INSERT INTO `object_size` (`object_size_id`, `object_size_description`) VALUES (1, 'small');
-INSERT INTO `object_size` (`object_size_id`, `object_size_description`) VALUES (2, 'medium');
-INSERT INTO `object_size` (`object_size_id`, `object_size_description`) VALUES (3, 'big');
